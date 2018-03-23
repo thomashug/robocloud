bb1abc86d5f61f56545d0a60b63574297bee1223
Thomas Hug <thomas.hug@atos.net>
2018-03-23 11:43:19 +0100

    Initial import

---
+++ src/main/kotlin/ch/atos/robocloud/DemoApplication.kt
@@ -0,0 +1,11 @@
+package ch.atos.robocloud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
